b521e5e39ea92246e5563c30e7e1b06baddbb19c
unknown <929571370@qq.com>
2018-04-25 00:24:56 +0800

    project init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,22 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    home
   </div>
+
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home',
+  data () {
+    return {
+
+    }
+  }
 }
 </script>
 
 <style>
+
 
 </style>
--- src/pages/home/List.vue
+++ src/pages/home/List.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div>List</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'List',
+  data () {
+    return {
+    }
+  }
 }
 </script>
 
