702b18e3c12c4158bb4e9dacd9e4d038d03e4255
huan_m <929571370@qq.com>
2018-05-10 16:36:32 +0800

    city alphabet

---
+++ src/pages/city/component/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="alphabet">
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">C</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  .alphabet
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    right 0
+    top 1.68rem
+    bottom 0
+    width .4rem
+    .item
+      line-height .4rem
+      text-align center
+      color $bgColor
+
+</style>
