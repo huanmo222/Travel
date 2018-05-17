69b7f7a274c60db503a794dc1fe4c8cf6c44730d
unknown <929571370@qq.com>
2018-05-17 23:24:34 +0800

    banner finish

---
+++ src/pages/detail/component/Banner.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="http://f.hiphotos.baidu.com/zhidao/pic/item/72f082025aafa40f6e86370bab64034f79f019a7.jpg">
+      <div class="banner-info">
+        <div class="banner-title">大连圣亚海洋世界(AAA景区)</div>
+        <div class="banner-number"><span class="iconfont banner-icon">&#xe692;&nbsp;</span>39</div>
+      </div>
+    </div>
+    <common-gallary
+      :imgs="imgs"
+      v-show="showGallary"
+      @closeGallray="handleGallrayClose"
+    ></common-gallary>
+  </div>
+
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      imgs: ['http://f.hiphotos.baidu.com/zhidao/pic/item/72f082025aafa40f6e86370bab64034f79f019a7.jpg', 'http://f.hiphotos.baidu.com/zhidao/pic/item/72f082025aafa40f6e86370bab64034f79f019a7.jpg'],
+      showGallary: false
+    }
+  },
+  components: {
+    CommonGallary
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallrayClose () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    background #ccc
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #fff
+      background-image linear-gradient(top,rgba(0, 0, 0, 0),rgb(0, 0, 0, 0.8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        height .32rem
+        line-height .32rem
+        margin-top .14rem
+        padding 0 .4rem
+        border-radius .2rem
+        background rgba(0, 0, 0, .8)
+        font-size .24rem
+        .banner-icon
+          font-size .24rem
+
+</style>
